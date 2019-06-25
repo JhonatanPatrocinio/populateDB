0f1e0183f3528ba802c7d26e3405d51b9cb33a18
Jhonatan Patrocinio <44841950+JhonatanPatrocinio@users.noreply.github.com>
2019-06-25 16:16:39 -0500

    add vendas

--- popular_db_vendas.py
+++ popular_db_vendas.py
@@ -75,12 +75,33 @@ def addAtendentes():
     )
     return add_atendentes
 
-def returnOneMunCodigo(allResults):
+def addVendas(cli_codigo, atendente_codigo, banco_codigo):
+    add_atendentes = (
+    "INSERT INTO vendas"
+    "(VEN_CLI_CODIGO, VEN_ATE_CODIGO, VEN_BAN_CODIGO, VEN_VALOR_TOTAL, VEN_FORMA_PAGAMENTO, VEN_OBSERVACOES)"
+    "VALUES (\'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\')" % (
+    cli_codigo,
+    atendente_codigo,
+    banco_codigo,
+    faker.pyfloat(left_digits=4, right_digits=2, positive=True, min_value=1, max_value=5000),
+    forma_pagamento(),
+    faker.text(max_nb_chars=255, ext_word_list=None)
+    )
+    )
+    return add_atendentes
+
+def returnOneResult(allResults):
     number = random.choice(allResults)[0]
     if number:
         return number
     else:
         raise AssertionError()
+
+def forma_pagamento():
+    formas_de_pagamento = ['D', 'A', 'C']
+    selected = random.choice(formas_de_pagamento)
+    print(selected)
+    return selected
 
 def fakerEndereco():
     s = '%s - %s' % (faker.street_address(), faker.bairro())
@@ -96,11 +117,10 @@ def fakerDateJoined():
     dateconverted = '{}'.format(date)
     return dateconverted
 
-
-
-
-
 query_mun_codigo = ("""SELECT MUN_CODIGO FROM municipios""")
+query_cliente_codigo = (""" SELECT CLI_CODIGO FROM clientes """)
+query_atendente_codigo = (""" SELECT ATE_CODIGO FROM atendentes """)
+query_banco_codigo = (""" SELECT BAN_CODIGO FROM bancos """)
 
 def main():
     try:
@@ -108,16 +128,20 @@ def main():
          user='root', passwd='1995', db='db_vendas', port=3306)
 
         cursor = cnx.cursor()
-        cnx.query(query_mun_codigo)
-        resultsMunicipios = cnx.store_result()
-        allResultsMunicipios = resultsMunicipios.fetch_row(maxrows=0)
+
         # ADD CLIENTE
-        # cliente_data = addClienteData(returnOneMunCodigo(allResultsMunicipios))
+        # cnx.query(query_mun_codigo)
+        # resultsMunicipios = cnx.store_result()
+        # allResultsMunicipios = resultsMunicipios.fetch_row(maxrows=0)
+        # cliente_data = addClienteData(returnOneResult(allResultsMunicipios))
         # print(cliente_data)
         # cursor.execute(cliente_data)
 
         # ADD FORNECEDOR
-        # fornecedor_data = addFornecedorData(returnOneMunCodigo(allResultsMunicipios))
+        # cnx.query(query_mun_codigo)
+        # resultsMunicipios = cnx.store_result()
+        # allResultsMunicipios = resultsMunicipios.fetch_row(maxrows=0)
+        # fornecedor_data = addFornecedorData(returnOneResult(allResultsMunicipios))
         # print(fornecedor_data)
         # cursor.execute(fornecedor_data)
 
@@ -127,10 +151,29 @@ def main():
         # cursor.execute(municipio_data)
 
         #ADD ATENDENTES
-        atendente_data = addAtendentes()
-        print(atendente_data)
-        cursor.execute(atendente_data)
-        cnx.commit()
+        # atendente_data = addAtendentes()
+        # print(atendente_data)
+        # cursor.execute(atendente_data)
+        # cnx.commit()
+
+        #ADD VENDAS
+        # cnx.query(query_cliente_codigo)
+        # resultsClientes = cnx.store_result()
+        # allResultsClientes = resultsClientes.fetch_row(maxrows=0)
+
+        # cnx.query(query_atendente_codigo)
+        # resultsAtendentes = cnx.store_result()
+        # allResultsAtendentes = resultsAtendentes.fetch_row(maxrows=0)
+
+        # cnx.query(query_banco_codigo)
+        # resultsBancos = cnx.store_result()
+        # allResultsBancos = resultsBancos.fetch_row(maxrows=0)
+
+        # venda_data = addVendas(returnOneResult(allResultsClientes), returnOneResult(allResultsAtendentes), returnOneResult(allResultsBancos))
+        # print(venda_data)
+        # cursor.execute(venda_data)
+        # cnx.commit()
+
 
     except MySQLdb.MySQLError as err:
         print(err)
